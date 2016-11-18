4edc6dc65eec69e2f875d2e5822b4dc95e927e7d
Vladimir <vladimir281277@gmail.com>
2016-11-18 16:43:34 +0200

    improving

--- receive.php
+++ receive.php
@@ -1,4 +1,5 @@
 <?php
+ini_set('error_reporting', ALL);
 
 include_once 'bd_conn.php';
 
@@ -11,8 +12,12 @@ if(isset($_POST['submit'])) {
 						INSERT INTO client(name, email) 
 						VALUES ('$name', '$email')
 						");
-
-mysql_close();
-
-echo "Successfully added!";
 }
+
+$result2 = mysql_query(" SELECT email FROM client WHERE email = '$email' ");
+$myrow = mysql_fetch_array($result2);
+
+if($myrow['email'] == $email) {
+	echo "Error";
+}
+
