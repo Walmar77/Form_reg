b0779cd1e02c7ebe4bd6bde3f9d1ecea0e5753d6
Vladimir <vladimir281277@gmail.com>
2016-11-25 11:34:38 +0200

    Done check email

--- receive.php
+++ receive.php
@@ -3,24 +3,23 @@ error_reporting (E_ALL);
 
 include 'db.php';
 
-if(isset($_POST['submit'])) {
+if(isset($_POST['email'])) {
 	$name = strip_tags(trim($_POST['name']));
 	$email = strip_tags(trim($_POST['email']));
 
 	$query = mysqli_query($dbhandle, "SELECT * FROM client WHERE email = '$email'");
 	$result = mysqli_fetch_array($query);
 
-		if($result['email'] == $email) {
+		if($result['email'] == $email) 
 			echo "Name: " . $result['name'] . "<br> E-mail: " . $result['email'];
-	}
 	
-}
+	
+ else {
 
-if(isset($_POST['submit'])) {
-	$name = strip_tags(trim($_POST['name']));
-	$email = strip_tags(trim($_POST['email']));
-
-	$sql = mysqli_query($dbhandle, "INSERT INTO client (name, email) VALUES ('$name', '$email')");
-}
+	mysqli_query($dbhandle, "INSERT INTO client (name, email) VALUES ('$name', '$email')");
+		die("<h4>Your data accepted!</h4>");
+	
+		}
+	}
 
 	
